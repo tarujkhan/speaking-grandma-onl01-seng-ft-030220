70ad3bfd30d694efabbb3636156a80e3afec3530
taroj khan <tarujkhan@yahoo.com>
2020-03-03 14:34:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,5 +16,10 @@ def speak_to_grandma(phrase)
   if phrase.upcase = "I LOVE YOU GRANDMA"
     puts "I LOVE YOU TOO PUMPKIN!"
     elsif
-    
+    puts "HUH?! SPEAK UP, SONNY!"
+  else
+    puts "NO, NOT SINCE 1938!"
+  end
+end
+
     
